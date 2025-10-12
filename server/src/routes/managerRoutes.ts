49a981d33bf6db413b88396cc01c1ac6a74ac4d1
samy884 <ssgill884@gmail.com>
2025-10-12 21:03:15 +0530

    disabling file imports

--- server/src/routes/managerRoutes.ts
+++ server/src/routes/managerRoutes.ts
@@ -2,16 +2,16 @@ import express from "express";
 import {
   createManager,
   getManager,
-  createManager,
-  updateManager,
-  getManagerProperties,
+  // createManager,
+  // updateManager,
+  // getManagerProperties,
 } from "../controllers/managerControllers";
 
 const router = express.Router();
 
 router.get("/:cognitoId", getManager);
-router.put("/:cognitoId", updateManager);
-router.get("/:cognitoId/properties", getManagerProperties);
+// router.put("/:cognitoId", updateManager);
+// router.get("/:cognitoId/properties", getManagerProperties);
 router.post("/", createManager);
 
 export default router;
