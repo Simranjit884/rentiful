b928f5f991c606844ab54043e76daef9d4faf522
samy884 <ssgill884@gmail.com>
2025-11-06 12:40:46 +0530

    added manager controllers and routes

--- server/src/routes/managerRoutes.ts
+++ server/src/routes/managerRoutes.ts
@@ -2,15 +2,14 @@ import express from "express";
 import {
   createManager,
   getManager,
-  // createManager,
-  // updateManager,
+  updateManager,
   // getManagerProperties,
 } from "../controllers/managerControllers";
 
 const router = express.Router();
 
 router.get("/:cognitoId", getManager);
-// router.put("/:cognitoId", updateManager);
+router.put("/:cognitoId", updateManager);
 // router.get("/:cognitoId/properties", getManagerProperties);
 router.post("/", createManager);
 
