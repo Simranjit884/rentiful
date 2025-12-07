d3e3351545a2ea32d01d716d7eddeaad67d3cc4b
samy884 <ssgill884@gmail.com>
2025-12-07 14:07:41 +0530

    added lease routes

--- server/src/controllers/leaseControllers.ts
+++ server/src/controllers/leaseControllers.ts
@@ -18,3 +18,20 @@ export const getLeases = async (req: Request, res: Response): Promise<void> => {
       .json({ message: `Error retrieving leases: ${error.message}` });
   }
 };
+
+export const getLeasePayments = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const payments = await prisma.payment.findMany({
+      where: { leaseId: Number(id) },
+    });
+    res.json(payments);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving lease payments: ${error.message}` });
+  }
+};
