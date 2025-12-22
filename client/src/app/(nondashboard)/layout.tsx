c0114d38270a92128d25cdb26647218cdaff9946
samy884 <ssgill884@gmail.com>
2025-12-22 16:23:27 +0530

    removing unnecassary background color

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -40,10 +40,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full w-full">
       <Navbar />
-      <main
-        className={`flex h-full w-full flex-col pt-[${NAVBAR_HEIGHT}px] bg-amber-600`}
-        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
-      >
+      <main className={`flex h-full w-full flex-col`} style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}>
         {children}
       </main>
     </div>
