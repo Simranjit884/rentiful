1c75637d5e2cf63ef5e81bc7815c49187fa17715
samy884 <ssgill884@gmail.com>
2026-01-01 22:33:21 +0530

    adding map and the location markers

--- client/src/app/(nondashboard)/search/page.tsx
+++ client/src/app/(nondashboard)/search/page.tsx
@@ -5,11 +5,10 @@ import { useAppDispatch, useAppSelector } from "@/state/redux";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect } from "react";
 import FiltersBar from "./FiltersBar";
-// import FiltersFull from "./FiltersFull";
 import { cleanParams } from "@/lib/utils";
 import { setFilters } from "@/state";
 import FiltersFull from "./FiltersFull";
-// import Map from "./Map";
+import Map from "./Map";
 // import Listings from "./Listings";
 
 const SearchPage = () => {
@@ -50,7 +49,7 @@ const SearchPage = () => {
         >
           <FiltersFull />
         </div>
-        {/* <Map /> */}
+        <Map />
         <div className="basis-4/12 overflow-y-auto">{/* <Listings /> */}</div>
       </div>
     </div>
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,7 +1,8 @@
-import { createNewUserInDatabase, withToast } from "@/lib/utils";
-import { Manager, Tenant } from "@/types/prismaTypes";
+import { cleanParams, createNewUserInDatabase, withToast } from "@/lib/utils";
+import { Manager, Property, Tenant } from "@/types/prismaTypes";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
+import { FiltersState } from ".";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
@@ -97,6 +98,41 @@ export const api = createApi({
         });
       },
     }),
+
+    // property related endpoints
+    getProperties: build.query<Property[], Partial<FiltersState> & { favoriteIds?: number[] }>({
+      query: (filters) => {
+        const params = cleanParams({
+          location: filters.location,
+          priceMin: filters.priceRange?.[0],
+          priceMax: filters.priceRange?.[1],
+          beds: filters.beds,
+          baths: filters.baths,
+          propertyType: filters.propertyType,
+          squareFeetMin: filters.squareFeet?.[0],
+          squareFeetMax: filters.squareFeet?.[1],
+          amenities: filters.amenities?.join(","),
+          availableFrom: filters.availableFrom,
+          favoriteIds: filters.favoriteIds?.join(","),
+          latitude: filters.coordinates?.[1],
+          longitude: filters.coordinates?.[0],
+        });
+
+        return { url: "properties", params };
+      },
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Properties" as const, id })),
+              { type: "Properties", id: "LIST" },
+            ]
+          : [{ type: "Properties", id: "LIST" }],
+      async onQueryStarted(_, { queryFulfilled }) {
+        await withToast(queryFulfilled, {
+          error: "Failed to fetch properties.",
+        });
+      },
+    }),
   }),
 });
 
@@ -104,4 +140,5 @@ export const {
   useGetAuthUserQuery,
   useUpdateTenantSettingsMutation,
   useUpdateManagerSettingsMutation,
+  useGetPropertiesQuery,
 } = api;
